7fdb9de51f7c5e0e827e44c582426353b641a05a
Yanis <yanis.baroudi@etu.umontpellier.fr>
2025-10-27 15:44:44 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,39 @@ import Menu from "./components/menu/Menu";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./components/contact/Contact";
 import Restaurant from "./components/restaurant/Restaurant";
+import { useEffect } from "react";
+
+const preloadImages = (imagePaths: string[]) => {
+  imagePaths.forEach((src) => {
+    const img = new Image();
+    img.src = new URL(src, import.meta.url).href;
+  });
+};
+
+
+export function usePreloadImages() {
+  useEffect(() => {
+    preloadImages([
+  "./assets/images/chef_icon.png",
+  "./assets/images/home_image.jpg",
+  "./assets/images/icon_navbar.png",
+  "./assets/images/plat1.jpg",
+  "./assets/images/plat2.jpg",
+  "./assets/images/plat3.jpg",
+  "./assets/images/plat4.jpg",
+  "./assets/images/plat5.jpg",
+  "./assets/images/plat6.jpg",
+  "./assets/images/plat7.jpg",
+  "./assets/images/plat8.jpg",
+  "./assets/images/plat9.jpg",
+  "./assets/images/plat10.jpg",
+  "./assets/images/table.jpg",
+]);
+  }, []);
+}
 
 function App() {
+  usePreloadImages();
   return (
     <Router>
       <div className="page">
